7bd375beab42ef03290f0a4b325f41f224031ecf
Denis Perov <denis.perov@gmail.com>
2022-02-27 22:45:30 +0300

    Add ESLint configuration

--- package.js
+++ package.js
@@ -4,14 +4,14 @@ Package.describe({
   summary: 'Meteor-Files API extended to work with MongoDB GridFS',
   git: 'https://github.com/majus/meteor-files-gridfs',
   documentation: 'README.md',
-})
+});
 
 Npm.depends({
   'abort-controller': '3.0.0',
-})
+});
 
 Package.onUse(function (api) {
-  api.versionsFrom('2.2')
+  api.versionsFrom('2.2');
   api.use([
     'ecmascript',
     'underscore',
@@ -20,13 +20,12 @@ Package.onUse(function (api) {
     'fetch',
     'mongo',
     'ostrio:files@2.0.1',
-  ])
-  api.mainModule('client/index.js', 'client')
-  api.mainModule('server/index.js', 'server')
-})
+  ]);
+  api.mainModule('client/index.js', 'client');
+  api.mainModule('server/index.js', 'server');
+});
 
 Package.onTest(function (api) {
-  api.use('ecmascript')
-  api.use('majus:files-gridfs')
-  //TODO: api.addFiles(...)
-})
+  api.use(['ecmascript', 'meteortesting:mocha']);
+  api.use('majus:files-gridfs');
+});
